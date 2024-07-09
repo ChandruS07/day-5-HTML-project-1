e9e2880fdeaf79f47198429edc7d2fb9a51ae119
ChandruS07 <srchandru13@gmail.com>
2024-07-09 15:46:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('off').addEventListener('click',function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2012/04/13/18/44/bulb-33238_640.png"
+});
+document.getElementById('on').addEventListener('click',function(){
+    document.getElementById("image").src="https://cdn.pixabay.com/photo/2014/04/03/10/41/light-311119_640.png"
+});
